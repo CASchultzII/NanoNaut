ee1dcb56b18cf97e6f74622fe7862742e440c951
CASchultzII <c.a.schultz.ii@gmail.com>
2016-09-21 22:14:21 -0600

    New enemy system and graphics!!

--- NanoNaut/scripts/assets.js
+++ NanoNaut/scripts/assets.js
@@ -6,10 +6,10 @@ var ASSETS = function(game) {
 
     this.load_assets = function() {
         this.game.load.image("SHIP", "assets/SpaceShip.png");
-        this.game.load.image("BULLET", "assets/Bullet.png");
-        this.game.load.image("ASTEROID_1", "assets/Asteroid_01.png");
-        this.game.load.image("ASTEROID_2", "assets/Asteroid_02.png");
-        this.game.load.image("ASTEROID_3", "assets/Asteroid_03.png");
+
+        this.game.load.image("BASIC_TRIPLE", "assets/enemies/GroupEnemy.png");
+        this.game.load.image("BASIC_SINGLE", "assets/enemies/BasicEnemy.png");
+
         this.game.load.image("BACKGROUND", "assets/background.png");
         this.game.load.image("BLUEPINKFONT", "assets/bluepinkfont.png");
         this.game.load.image("BAR", "assets/Bar.png");
